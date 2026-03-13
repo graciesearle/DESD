6dc1155cef020842ed6bf635fea3716765b8ed16
Mickelll22 <mickelgerges1@gmail.com>
2026-03-13 17:32:46 +0000

    added available all year round option

--- products/serializers.py
+++ products/serializers.py
@@ -20,6 +20,6 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'producer', 'name', 'description', 'price', 'unit',
             'stock_quantity', 'image', 'category', 'category_name', 'farm', 'farm_name', 'farm_postcode', 
-            'is_available', 'allergens', 'allergen_names', 'season_start', 'season_end', 'created_at', 'updated_at',
+            'is_available', 'allergens', 'allergen_names', 'is_year_round', 'season_start', 'season_end', 'created_at', 'updated_at',
         ]
         read_only_fields = ['id', 'created_at', 'updated_at']
